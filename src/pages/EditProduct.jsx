1043fc7e72baa52443579da6edf4b8b5c7be00cc
Anurag <danurag1906@gmail.com>
2023-10-09 19:13:32 +0530

    fixed more typos

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -50,7 +50,7 @@ const EditProduct = () => {
   return (
     <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl my-4">Create Product</h1>
+      <h1 className="text-3xl my-4">Edit Product</h1>
       {loading ? <Spinner /> : ""}
       <div className="flex flex-col border-2 border-sky-400 roundex-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
