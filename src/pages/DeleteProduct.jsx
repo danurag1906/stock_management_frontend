c4376800c2c9d038b1f6020eca496e97907020e4
Anurag <danurag1906@gmail.com>
2023-10-09 19:11:08 +0530

    fixed typos

--- src/pages/DeleteProduct.jsx
+++ src/pages/DeleteProduct.jsx
@@ -32,7 +32,7 @@ const DeleteProduct = () => {
   return (
     <div className='p-4' >
         <BackButton/>
-        <h1 className='text-3xl my-4' >Delete Book</h1>
+        <h1 className='text-3xl my-4' >Delete Product</h1>
         {loading?<Spinner/>:''}
         <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 m-auto' >
             <h3 className='text-2xl'> Are you sure you want to delete this product?</h3>
